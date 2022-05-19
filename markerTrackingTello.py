cd53188f4d80644ca0de145b0c42c5d3accea281
Matan-Hodadov <matanhod1@gmail.com>
2022-05-19 18:52:43 +0300

    fixing drone reactions

--- markerTrackingTello.py
+++ markerTrackingTello.py
@@ -2,9 +2,7 @@ from utils2 import *
 import cv2
 import pandas as pd
 import keyboard
-import logging
-from threading import Thread
-
+# from threading import Thread
 
 w, h = 360, 240
 pid = [0.6, 0.45, 0]
@@ -15,58 +13,55 @@ pErrorh = 0
 startCounter = 0  # for no Flight 1   - for flight 0
 loop_counter = 0
 
-
-# create logging
-logging.basicConfig(filename='TelloLogFile.log', filemode='w', level=logging.INFO)
-out = cv2.VideoWriter('TelloVideoRecorder.avi', -1, 20.0, (640,480))
-
 myDrone = initializeTello()
 print(myDrone.get_battery())
 
+# create logging
+with open('logging.txt', 'w') as f:
+    # try:
+    while True:
 
-# try:
-while True:
+        # Flight
+        if startCounter == 0:
+            myDrone.takeoff()
+            myDrone.move_up(20)
+            f.write("Take off\n")
+            startCounter = 1
 
-    # Flight
-    if startCounter == 0:
-        myDrone.takeoff()
-        logging.info("Take off")
-        startCounter = 1
+        # Step 1
+        img = telloGetFrame(myDrone, w, h)
+        # if myDrone.get_height() < 15:
+        #     myDrone.land()
+        #     break
 
-    # Step 1
-    img = telloGetFrame(myDrone, w, h)
-    # if myDrone.get_height() < 15:
-    #     myDrone.land()
-    #     break
-    # Step 2
-    img, centers, cordinates, areas = findMarker(img, 33, "DICT_ARUCO_ORIGINAL")
-    if len(centers) > 0:
-        pError, keepRecording = trackMarker(myDrone, centers[0][0], centers[0][1], areas[0], w, h, pError)
-    else:
-        pError, keepRecording = trackMarker(myDrone, 0, 0, 0, w, h, pError)
+        # Step 2
+        img, centers, cordinates, areas = findMarker(img, 33, "DICT_ARUCO_ORIGINAL")
+        if len(centers) > 0:
+            # pError, keepRecording = trackMarker(myDrone, centers[0][0], centers[0][1], areas[0], w, h, pError)
+            pError = trackMarker(myDrone, centers[0][0], centers[0][1], areas[0], w, h, pError)
+        else:
+            # pError, keepRecording = trackMarker(myDrone, 0, 0, 0, w, h, pError)
+            pError = trackMarker(myDrone, 0, 0, 0, w, h, pError)
 
-    # if drone land and we finished
-    if not keepRecording:
-        myDrone.land()
-        logging.info("Land")
-        print(myDrone.get_battery())
-        out.release()
-        break
+        # if drone land and we finished
+        if not myDrone.is_flying:
+            f.write("Landing\n")
+            print(myDrone.get_battery())
+            break
 
-    if loop_counter == 100:
-        logging.info("Battery:", myDrone.get_battery(), "Height:", myDrone.get_height(), "X Speed:",
-                     myDrone.get_speed_x(), "Y Speed:", myDrone.get_speed_y(),
-                     "Z Speed:", myDrone.get_speed_z(), "Temperature:", myDrone.get_temperature(), "Barometer:",
-                     myDrone.get_barometer())
-    else:
-        loop_counter += 1
+        if loop_counter == 100:
+            f.write("Drone info:\nBattery: " + str(myDrone.get_battery()) + " Height: " + str(myDrone.get_height()) + " X Speed: " +
+                    str(myDrone.get_speed_x()) + " Y Speed: " + str(myDrone.get_speed_y()) +
+                    " Z Speed: " + str(myDrone.get_speed_z()) + " Temperature: " + str(myDrone.get_temperature()) + " Barometer: " +
+                    str(myDrone.get_barometer()) + "\n\n")
+        else:
+            loop_counter += 1
 
-    cv2.imshow('Image', img)
-    out.write(img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        myDrone.land()
-        logging.info("Land")
-        print(myDrone.get_battery())
-        out.release()
-        break
-
+        cv2.imshow('Image', img)
+        # out.write(img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            myDrone.land()
+            f.write("Landing\n")
+            print(myDrone.get_battery())
+            # out.release()
+            break
--- utils2.py
+++ utils2.py
@@ -17,9 +17,13 @@ up = 0
 down = 0
 
 pError = 0
-fbRange = [4000, 6500]  # about [60, 130] cm
-edge_dist = [4, 8]
+fbRange = [1180, 1230]  # about [60, 130] cm
+fbRange2 = [4000, 6500]  # about [60, 130] cm
+down_up_dist = [4, 8]
 left_right_curved = [2, 4]
+
+priorityID = 23
+priorityType = "DICT_6X6_100"
 
 
 def initializeTello():
@@ -72,6 +76,8 @@ def findMarker(img, givenId: int, type: str):  # id example: "DICT_ARUCO_ORIGINA
     ap = argparse.ArgumentParser()
     ap.add_argument("-i", "--image", required=False, help="path to input image containing ArUCo tag")
     ap.add_argument("-t", "--type", type=str, default=type, help="type of ArUCo tag to detect")
+    # ap.add_argument("-t", "--type", type=str, default=priorityType, help="type2 of ArUCo tag to detect")
+
     args = vars(ap.parse_args())
     # verify that the supplied ArUCo tag exists and is supported by
     # OpenCV
@@ -103,7 +109,7 @@ def findMarker(img, givenId: int, type: str):  # id example: "DICT_ARUCO_ORIGINA
             # extract the marker corners (which are always returned
             # in top-left, top-right, bottom-right, and bottom-left
             # order)
-            if markerID == givenId:
+            if markerID == priorityID:
                 corners = markerCorner.reshape((4, 2))
                 (topLeft, topRight, bottomRight, bottomLeft) = corners
 
@@ -155,25 +161,78 @@ def findMarker(img, givenId: int, type: str):  # id example: "DICT_ARUCO_ORIGINA
                 # draw the ArUco marker ID on the frame
                 cv2.putText(frame, str(markerID),
                             (topLeft[0], topLeft[1] - 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+                break
+            elif markerID == givenId:
+                corners = markerCorner.reshape((4, 2))
+                (topLeft, topRight, bottomRight, bottomLeft) = corners
+
+                # convert each of the (x, y)-coordinate pairs to integers
+                topRight = (int(topRight[0]), int(topRight[1]))
+                bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
+                bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
+                topLeft = (int(topLeft[0]), int(topLeft[1]))
+
+                # AREA
+                # print("-------")
+                area = int(abs(math.dist(topRight, topLeft)) * abs(math.dist(topLeft, bottomLeft)))
+                # print(area)
+                # global right
+                right = int(abs(math.dist(topRight, bottomRight)))
+                # global left
+                left = int(abs(math.dist(topLeft, bottomLeft)))
+                # global up
+                up = int(abs(math.dist(topLeft, topRight)))
+                # global down
+                down = int(abs(math.dist(bottomLeft, bottomRight)))
+
+                # draw the bounding box of the ArUCo detection
+                cv2.line(frame, topLeft, topRight, (0, 255, 0), 2)
+                cv2.line(frame, topRight, bottomRight, (0, 255, 0), 2)
+                cv2.line(frame, bottomRight, bottomLeft, (0, 255, 0), 2)
+                cv2.line(frame, bottomLeft, topLeft, (0, 255, 0), 2)
+
+                # compute and draw the center (x, y)-coordinates of the ArUco marker
+                cX = int((topLeft[0] + bottomRight[0]) / 2.0)
+                cY = int((topLeft[1] + bottomRight[1]) / 2.0)
+                cv2.circle(frame, (cX, cY), 4, (0, 0, 255), -1)
+
+                # PRINT DOTS POS
+                cv2.circle(frame, (topLeft[0], topLeft[1]), 4, (255, 255, 255), -1)
+                cv2.putText(frame, "TL",
+                            (topLeft[0] - 15, topLeft[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+                cv2.circle(frame, (topRight[0], topRight[1]), 4, (255, 255, 255), -1)
+                cv2.putText(frame, "TR",
+                            (topRight[0] + 15, topRight[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+                cv2.circle(frame, (bottomRight[0], bottomRight[1]), 4, (255, 255, 255), -1)
+                cv2.putText(frame, "BR",
+                            (bottomRight[0], bottomRight[1] + 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+                # saving markers info in center and cordi
+                cordinates.append([topLeft, topRight, bottomRight, bottomLeft])
+                centers.append([cX, cY])
+                areas.append(area)
+                # draw the ArUco marker ID on the frame
+                cv2.putText(frame, str(markerID),
+                            (topLeft[0], topLeft[1] - 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
 
         # show the output frame
         # cv2.imshow("Frame", frame)
         # key = cv2.waitKey(1) & 0xFF
-        # if the `q` key was pressed, break from the loop
+        # # if the `q` key was pressed, break from the loop
         # if key == ord("q"):
         # break
     return frame, centers, cordinates, areas
-    # do a bit of cleanup
-    # cv2.destroyAllWindows()
-    # vs.stop()
 
 
 def trackMarker(myDrone, x, y, area, w, h, pError):
     print("-----new loop-----")
-    print("are is:", area)
     flag = True
-    keepRecording = True
-
+    # #inside func
+    # def change_flag_true() -> bool:
+    #     if flag == false:
+    #         return
+    #     flag = true
     error = x - w // 2
     speed = pid[0] * error + pid[1] * (error - pError)
     speed = int(np.clip(speed, -100, 100))
@@ -182,59 +241,75 @@ def trackMarker(myDrone, x, y, area, w, h, pError):
     if fbRange[0] < area < fbRange[1]:
         myDrone.for_back_velocity = 0
         print(area)
+        # flag = change_flag_true()
     elif area > fbRange[1]:
-        # dynamic_spd = -1 * int(((area - fbRange[1]) / (10000-fbRange[1])) * 100)
-        dynamic_spd = -1 * int(math.pow((area - fbRange[1]) / (10000-fbRange[1]), 2))
+        # dynamic_spd = -1 * int(math.pow((area - fbRange[1]) / (10000-fbRange[1]), 2))
+        dynamic_spd = -1 * int((area - fbRange[1]) / 20)
         print("need to go back, area is:", area)
         print("dynamic speed is:", dynamic_spd)
-        if dynamic_spd < -40:
-            dynamic_spd = -40
-        if dynamic_spd > -20:
-            dynamic_spd = -20
+        if dynamic_spd < -50:
+            dynamic_spd = -50
+        if 0 > dynamic_spd < -25:
+            dynamic_spd = -25
         myDrone.for_back_velocity = dynamic_spd
         flag = False
         # print("flag changed in line 187")
     elif area < fbRange[0] and area != 0:
         # dynamic_spd = int(math.pow((fbRange[0]- area)/(fbRange[0]/10), 2))
-        dynamic_spd = int(math.pow((fbRange[0] - area)/(fbRange[0]/6.3095), 2.5))
+        dynamic_spd = int(math.pow((fbRange[0] - area)/(fbRange[0]/7.5), 2.5))
         if dynamic_spd > 100:
             dynamic_spd = 100
-        myDrone.for_back_velocity = dynamic_spd
+        myDrone.for_back_velocity = int(0.6 * dynamic_spd)
+        # print("area:", area)
+        # print("dynamic speed:", dynamic_spd)
+        # print("current speed:", myDrone.get_speed_x())
         flag = False
         # print("flag changed in line 191")
     else:
         myDrone.for_back_velocity = 0
+        # flag = change_flag_true()
 
     # up and down
     if y > h // 2 - 10 and y < h // 2 + 10:
         myDrone.up_down_velocity = 0
+        # flag = change_flag_true()
     elif y > h // 2 + 10:
-        myDrone.up_down_velocity = -20
+        myDrone.up_down_velocity = int(-math.pow(((y - h // 2 + 10)//3), 1.3) )# -20
         flag = False
         # print("flag changed in line 203")
     elif y < h // 2 - 10 and y != 0:
-        myDrone.up_down_velocity = 20
+        myDrone.up_down_velocity =int( math.pow((((h // 2-10-y)//3)), 1.3))
         flag = False
         # print("flag changed in line 207")
     else:
         myDrone.up_down_velocity = 0
         # flag = change_flag_true()
 
-    # right left curved
-    if left - right > 4:  # TODO needs to change it to left right curved (just like edge_dist)
-        # print("need to move right")
-        # myDrone.move_right(2*(left-right))
-        myDrone.left_right_velocity = 10
-        flag = False
-        # print("flag changed in line 216")
-    elif right - left > 4:
-        # print("need to move left")
-        # myDrone.move_left(2 * (left - right))
-        myDrone.left_right_velocity = -10
-        flag = False
-        # print("flag changed in line 222")
+    # # right left curved
+    # if left-right > 0:
+    #     # print("need to move right")
+    #     # myDrone.move_right(2*(left-right))
+    #     myDrone.left_right_velocity = -10
+    #     flag = False
+    #     # print("flag changed in line 216")
+    # elif right - left > 0:
+    #     # print("need to move left")
+    #     # myDrone.move_left(2 * (left - right))
+    #     myDrone.left_right_velocity = 10
+    #     flag = False
+    #     # print("flag changed in line 222")
+    # else:
+    #     myDrone.left_right_velocity = 0
+    #     # change_flag_true()
+
+    # better left right curved
+    lfc = left-right
+    if lfc > 4:
+        myDrone.left_right_velocity = 40
+    elif lfc < -4:
+        myDrone.left_right_velocity = -40
     else:
-        myDrone.left_right_velocity = 0
+        myDrone.left_right_velocity = lfc*-10
 
     # spin
     if x != 0:
@@ -243,32 +318,133 @@ def trackMarker(myDrone, x, y, area, w, h, pError):
         # print("flag changed in line 233")
     else:
         myDrone.yaw_velocity = 0
-
     if myDrone.send_rc_control:
         myDrone.send_rc_control(myDrone.left_right_velocity,
                                 myDrone.for_back_velocity,
                                 myDrone.up_down_velocity,
                                 myDrone.yaw_velocity)
-
     # up down curved
     print("down-up = ", (down-up))
-    if fbRange[0] < area < ((fbRange[1] - fbRange[0])/2) + fbRange[0]:
-        if down - up > edge_dist[0]:
-            print("need to land")
-            if flag is True:
-                print("landing")
-                myDrone.land()
-                keepRecording = False
-            else:
-                print("cant land")
+    if fbRange[0] < area < ((fbRange[1] - fbRange[0]) / 2) + fbRange[0]:
+        downUpDist = down_up_dist[0]
     else:
-        if down - up > edge_dist[1]:
-            print("need to land")
-            if flag is True:
-                print("landing")
-                myDrone.land()
-                keepRecording = False
-            else:
-                print("cant land")
+        downUpDist = down_up_dist[1]
+    if down - up > downUpDist:
+        print("need to land")
+        if flag is True:
+            print("landing")
+            myDrone.land()
+        else:
+            print("cant land")
+    return error
 
-    return error, keepRecording
+
+
+
+# def trackMarker(myDrone, x, y, area, w, h, pError):
+#     print("-----new loop-----")
+#     # print("area is:", area)
+#     flag = True
+#     # keepRecording = True
+#
+#     error = x - w // 2
+#     speed = pid[0] * error + pid[1] * (error - pError)
+#     speed = int(np.clip(speed, -100, 100))
+#
+#     # forward and backward
+#     if fbRange[0] < area < fbRange[1]:
+#         myDrone.for_back_velocity = 0
+#         # print(area)
+#     elif area > fbRange[1]:
+#         # dynamic_spd = -1 * int(((area - fbRange[1]) / (10000-fbRange[1])) * 100)
+#         dynamic_spd = -1 * int(math.pow((area - fbRange[1]) / (10000-fbRange[1]), 2))
+#         # print("need to go back, area is:", area)
+#         # print("dynamic speed is:", dynamic_spd)
+#         if dynamic_spd < -40:
+#             dynamic_spd = -40
+#         elif dynamic_spd > -20:
+#             dynamic_spd = -20
+#         myDrone.for_back_velocity = dynamic_spd
+#         flag = False
+#         # print("flag changed in line 187")
+#     elif area < fbRange[0] and area != 0:
+#         # dynamic_spd = int(math.pow((fbRange[0]- area)/(fbRange[0]/10), 2))
+#         dynamic_spd = int(math.pow((fbRange[0] - area)/(fbRange[0]/6.3095), 2.5))
+#         if dynamic_spd > 100:
+#             dynamic_spd = 100
+#         myDrone.for_back_velocity = dynamic_spd
+#         flag = False
+#         # print("flag changed in line 191")
+#     else:
+#         myDrone.for_back_velocity = 0
+#
+#     # up and down
+#     if h // 2 - 10 < y < h // 2 + 10:
+#         myDrone.up_down_velocity = 0
+#     elif y > h // 2 + 10:
+#         myDrone.up_down_velocity = -20
+#         flag = False
+#         # print("flag changed in line 203")
+#     elif y < h // 2 - 10 and y != 0:
+#         myDrone.up_down_velocity = 20
+#         flag = False
+#         # print("flag changed in line 207")
+#     else:
+#         myDrone.up_down_velocity = 0
+#         # flag = change_flag_true()
+#
+#     # right left curved
+#     if left - right > 4:  # TODO needs to change it to left right curved (just like edge_dist)
+#         # print("need to move right")
+#         # myDrone.move_right(2*(left-right))
+#         myDrone.left_right_velocity = 10
+#         flag = False
+#         # print("flag changed in line 216")
+#     elif right - left > 4:
+#         # print("need to move left")
+#         # myDrone.move_left(2 * (left - right))
+#         myDrone.left_right_velocity = -10
+#         flag = False
+#         # print("flag changed in line 222")
+#     else:
+#         myDrone.left_right_velocity = 0
+#
+#     # spin
+#     if x != 0:
+#         myDrone.yaw_velocity = speed
+#         # flag = False
+#         # print("flag changed in line 233")
+#     else:
+#         myDrone.yaw_velocity = 0
+#
+#         if myDrone.send_rc_control:
+#             myDrone.send_rc_control(myDrone.left_right_velocity,
+#                                     myDrone.for_back_velocity,
+#                                     myDrone.up_down_velocity,
+#                                     myDrone.yaw_velocity)
+#
+#     if fbRange[0] < area < ((fbRange[1] - fbRange[0])/2) + fbRange[0]:
+#         if down - up > edge_dist[0]:
+#             print("need to land")
+#             if flag is True:
+#                 print("landing")
+#                 myDrone.land()
+#                 with open('logging.txt', 'a') as f:
+#                     f.write("Landing")
+#                 # keepRecording = False
+#             else:
+#                 print("cant land")
+#     elif fbRange[0] < area < fbRange[1] - ((fbRange[1] - fbRange[0])/2):
+#         if down - up > edge_dist[1]:
+#             print("need to land")
+#             if flag is True:
+#                 print("landing")
+#                 myDrone.land()
+#                 with open('logging.txt', 'a') as f:
+#                     f.write("Landing")
+#             # keepRecording = False
+#             else:
+#                 print("cant land")
+#
+#     # return error, keepRecording
+#     return error
